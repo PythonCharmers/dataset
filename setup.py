d1d72ed7055b87652b6a3d8de376985c56d87684
Ed Schofield <ed@pythoncharmers.com>
2024-07-04 13:55:35 +1000

    Mark SQLAlchemy 2.0.x as being supported in setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     include_package_data=False,
     zip_safe=False,
     install_requires=[
-        "sqlalchemy >= 1.4.0",
+        "sqlalchemy >= 1.3.2",
         "alembic >= 0.6.2",
         "banal >= 1.0.1",
     ],
